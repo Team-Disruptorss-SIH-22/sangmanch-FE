d94867d3ac21d9f127b98d374a4ab23631467b8b
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2022-03-12 14:42:22 +0530

    Revert "Add Routes"

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AdminLogin, Home } from "../pages/index";
-
 import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/admin-login" element={<AdminLogin/>}/>
-        </Routes>
-      </Router>
-
+      Hello World
     </div>
   );
 }
--- src/pages/AdminLogin.js
+++
@@ -1,5 +0,0 @@
-const AdminLogin = () => {
-  return <div>Admin Login Page</div>;
-};
-
-export default AdminLogin;
