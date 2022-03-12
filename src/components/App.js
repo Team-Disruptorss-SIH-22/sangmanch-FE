8d7bc3935f4907d0eea38c65048f8eec1f0ab1d8
Indrakant Dana <ik.dana24@gmail.com>
2022-03-12 15:06:03 +0530

    Add Pages, Routes, Components
    - Setup Home Page, Admin Login Page. Exporting from index.js
    - Setup Routes in App.js
    - Shifted App.js to Components Folder.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AdminLogin, Home } from "../pages/index";
+
 import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/admin-login" element={<AdminLogin/>}/>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/pages/AdminLogin.js
@@ -0,0 +1,5 @@
+const AdminLogin = () => {
+  return <div>Admin Login Page</div>;
+};
+
+export default AdminLogin;
