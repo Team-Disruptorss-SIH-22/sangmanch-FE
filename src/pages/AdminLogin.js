1f5d6c9c87c26a23504efb629b01e7ad18c690bc
Indrakant Dana <ik.dana24@gmail.com>
2022-03-12 17:46:47 +0530

    Create Admin Login Page Static
    - Created the HTML & CSS part of Admin Login Page.

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/adminlogin.module.css";
+
 const AdminLogin = () => {
-  return <div>Admin Login Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginPageContainer}>
+        <div className={styles.logoIcon}>
+          <img
+            src="https://narcoticsindia.nic.in/images/acs.png"
+            alt="logo-icon"
+          />
+        </div>
+
+        <div className={styles.headers}>
+          <p className={styles.title}>NCB DAAS</p>
+          <p className={styles.titleDetails}>
+            Drugs Analytics and Alerting System
+          </p>
+          <p className={styles.loginTitle}>Log In</p>
+        </div>
+
+        <div className={styles.formContainer}>
+          <div className={styles.inputContainer}>
+            <label for="email">EMAIL</label>
+            <input type="email" name="email" placeholder="Email Address" />
+          </div>
+          <div className={styles.inputContainer}>
+            <div className={styles.forPassword}>
+              <label for="password">PASSWORD</label>
+              <Link to="/" className={styles.forgotPassword}><p>Forgot Password?</p></Link>
+            </div>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <button className={styles.submit}>Log In</button>
+        </div>
+
+        <div className={styles.footer}>
+          <p>Don't Have an Account? <Link to="/">Sign up</Link></p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLogin;
