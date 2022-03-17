0ab0d217b22aecf20335b13bed1835c64d19114e
harshitkathuria <harshitkathuria14@gmail.com>
2022-03-17 19:21:12 +0530

    Add Landing Page

--- src/components/pages/Homepage/Home.js
+++ src/components/pages/Homepage/Home.js
@@ -1,57 +1,42 @@
 import React from "react";
 
 import HomeNavbar from "./HomeNavbar";
-import Img1 from "../../../assets/home/img1.png";
-import Img2 from "../../../assets/home/img2.png";
-import Img3 from "../../../assets/home/img3.png";
-import Img4 from "../../../assets/home/img4.png";
+import Footer from "../Footer";
+import img__1 from "../../../assets/home/img1.png";
+import img__2 from "../../../assets/home/img2.png";
+import img__3 from "../../../assets/home/img3.png";
+import img__4 from "../../../assets/home/img4.png";
 
-import styles from "../../../styles/home.module.css";
+import styles from "../../../styles/Landing/home.module.css";
 
 const Home = () => {
 	return (
 		<div className={styles.container}>
 			<HomeNavbar />
-			<div className={styles.content_Container}>
-				<div className={styles.leftbox}>
-					<img src={Img1} className={styles.img1} alt="" />
-					<div className={styles.info}>
-						<div className={styles.title}>
-							<b>
-								<h1>
-									<p className={styles.title}>Help Us In Tracking</p>
-									<p className={styles.title}> Illegal Drugs</p>
-								</h1>
-							</b>
+			<div className={styles.left}>
+				<div className={styles.image__container}>
+					<img src={img__1} alt="" className={styles.img__1} />
+				</div>
+				<div>
+					<div className={styles.showcase__text}>
+						<div className={styles.heading}>
+							<p>Help Us In Tracking</p>
+							<p className={styles.text__primary}>Illegal Drug Sales</p>
 						</div>
-
-						<p>
+						<div className={styles.info}>
 							DAAS (Drug Analysis and Alerting System) is a nationalized framework to
 							track the procurement of the drugs described as prescription required drugs
 							in the NDPS act 1985. The data collected is stored in secured government
-							channels and monitored for abnormal sales
-						</p>
-
-						<button className={styles.but}>
-							{" "}
-							<span>Learn more</span>
-						</button>
+							channels and monitored for abnormal sales.
+						</div>
 					</div>
+					<div className={styles.button}>Learn More</div>
 				</div>
-
-				<div className={styles.middlebox}>
-					<img className={styles.img2} src={Img2} width="300 px" alt="" />
-				</div>
-
-				<div className={styles.rightbox}>
-					<div className={styles.imageContainer}>
-						<img className={styles.img3} src={Img3} alt="" />
-					</div>
-					<div className={styles.imageContainer + " " + styles.img4}>
-						<img className={styles.img4} src={Img4} alt="" />
-					</div>
-				</div>
+				<img src={img__2} className={styles.img__2} alt="" />
+				<img src={img__3} className={styles.img__3} alt="" />
+				<img src={img__4} className={styles.img__4} alt="" />
 			</div>
+			<Footer />
 		</div>
 	);
 };
