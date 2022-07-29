f21df6d2d23f4550d71f63c4fc8481e2d5125716
Indrakant Dana <ik.dana24@gmail.com>
2022-07-29 20:26:55 +0530

    Setup basic Events page and styling

--- src/components/pages/User/Events.js
+++ src/components/pages/User/Events.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from '../../../styles/user/events.module.css';
 
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className={styles.eventsContainer}>
+      <div className={styles.eventsHeaderContainer}>
+        <p>Event Information</p>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
